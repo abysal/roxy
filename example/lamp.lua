2e28ed3a0a77b32fd34ea5fff386497e6756ad06
Akashi <akashic.records.of.the.abyss@gmail.com>
2026-03-04 21:02:59 +0000

    Initial commit

---
+++ example/lamp.lua
@@ -0,0 +1,16 @@
+block "wiki:lamp" {
+    category = "items",
+    display_name = "Lamp",
+
+    lighting {
+        emission = 15,
+        dampening = 0,
+    },
+
+    map_color = hex "#FFAABB",
+
+    textures {
+        default = "textures/blocks/wiki_lamp"
+    }
+}
+
---
+++ lua/api.lua
@@ -0,0 +1,37 @@
+---@param identifier string
+---@return function(body: table): nil
+function block(identifier)
+end
+
+---@param table table
+---@return table
+function lighting(table)
+end
+---@param table table
+---@return table
+function culling(table)
+end
+---@param table table
+---@return table
+function textures(table)
+end
+---@param table table
+---@return table
+function geometry(table)
+end
+---@param table table
+---@return table
+function bone_visibility(table)
+end
+
+---@param r number
+---@param g number
+---@param b number
+---@return table
+function rgb(r, g, b)
+end
+
+---@param hex string
+---@return table
+function hex(hex)
+end
